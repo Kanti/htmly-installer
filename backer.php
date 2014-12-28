34f0fbbdde27fc602dd4026fa49fb0a49fcfc49d
Kanti <git@kanti.de>
2014-12-28 22:31:28 +0100

    [TASK] added method Backer->addDir

--- backer.php
+++ backer.php
@@ -1,44 +1,51 @@
 <?php
 
-class Backer {
+class Backer
+{
 
-	protected $backedName = "htmly-installer";
+    protected $backedName = "htmly-installer";
 
-	protected $fileList = array();
+    protected $fileList = array();
 
-	public function addFile($fileName) {
-		$this->fileList[] = $fileName;
-		return $this;
-	}
-	
-	protected function testIfNecessary()  {
-		
-		if(! file_exists(dirname($_SERVER["SCRIPT_FILENAME"]) . "/" . "composer.json"))
-			return false;
-		
-		$time = filemtime(dirname($_SERVER["SCRIPT_FILENAME"]) . "/" . $this->backedName . ".php");
-		
-		foreach($this->fileList as $fileName) {
-			if($time < filemtime(dirname($_SERVER["SCRIPT_FILENAME"]) . "/" . $fileName))
-				return true;
-		}
-		return false;
-	}
+    public function addFile($fileName)
+    {
+        $this->fileList[] = $fileName;
+        return $this;
+    }
 
-	public function __construct(){
-	
-		$this->addFile("vendor/autoload.php");
+    public function addDir($dirName)
+    {
+        $dirName = rtrim($dirName,"/") . "/";
+        foreach (scandir($dirName) as $key => $value) {
+            if (!in_array($value, array(".", ".."))) {
+                if (is_dir($dirName . $value)) {
+                    $this->addDir($dirName . $value);
+                } else {
+                    $this->addFile($dirName . $value);
+                }
+            }
+        }
+        return $this;
+    }
 
-		$this->addFile("vendor/composer/autoload_real.php")
-			->addFile("vendor/composer/autoload_namespaces.php")
-			->addFile("vendor/composer/autoload_psr4.php")
-			->addFile("vendor/composer/autoload_classmap.php")
-			->addFile("vendor/composer/ClassLoader.php");
+    protected function testIfNecessary()
+    {
 
-		$this->addFile("vendor/kanti/hub-updater/HubUpdater.php")
-			->addFile("vendor/kanti/hub-updater/HelperClass.php")
-			->addFile("vendor/kanti/hub-updater/CacheOneFile.php")
-			->addFile("vendor/kanti/hub-updater/ca_bundle.crt");
+        if (!file_exists(dirname($_SERVER["SCRIPT_FILENAME"]) . "/" . "composer.json"))
+            return false;
+
+        $time = filemtime(dirname($_SERVER["SCRIPT_FILENAME"]) . "/" . $this->backedName . ".php");
+
+        foreach ($this->fileList as $fileName) {
+            if ($time < filemtime(dirname($_SERVER["SCRIPT_FILENAME"]) . "/" . $fileName))
+                return true;
+        }
+        return false;
+    }
+
+    public function __construct()
+    {
+        $this->addDir("vendor/");
 
         $this->addFile("index.php")
             ->addFile("backer.php");
@@ -51,32 +58,34 @@ class Backer {
             ->addFile("old/src/Message.php")
             ->addFile("old/src/Settings.php");
 	    */
-	}
-	
-	public function run() {
-		if($this->testIfNecessary()){
-			$this->back();
-			return true;
-		}
-		return false;
-	}
-	
-	protected function back() {
-		$phar = new Phar(
-			$this->backedName . ".phar", 
-			FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME, 
-			$this->backedName . ".phar");
+    }
 
-		foreach($this->fileList as $fileName) {
-			$phar[$fileName] = file_get_contents(dirname($_SERVER["SCRIPT_FILENAME"]) . "/" . $fileName);
-		}
-		$phar->setStub($phar->createDefaultStub("index.php"));
+    public function run()
+    {
+        if ($this->testIfNecessary()) {
+            $this->back();
+            return true;
+        }
+        return false;
+    }
 
-		copy($this->backedName . ".phar",$this->backedName . ".php");
-		//$phar->stopBuffering();
-		unset($phar);
-		unlink($this->backedName . ".phar");
-	}
+    protected function back()
+    {
+        $phar = new Phar(
+            $this->backedName . ".phar",
+            FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME,
+            $this->backedName . ".phar");
+
+        foreach ($this->fileList as $fileName) {
+            $phar[$fileName] = file_get_contents(dirname($_SERVER["SCRIPT_FILENAME"]) . "/" . $fileName);
+        }
+        $phar->setStub($phar->createDefaultStub("index.php"));
+
+        copy($this->backedName . ".phar", $this->backedName . ".php");
+        unset($phar);
+        unlink($this->backedName . ".phar");
+    }
 }
+
 $backer = (new Backer);
-return( $backer->run() );
+return ($backer->run());
