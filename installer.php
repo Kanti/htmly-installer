2e78e312c693ab2a403cc6946ce5e7fd750f4f41
Kanti <git@kanti.de>
2015-07-17 13:45:36 +0200

    [BUGFIX] site.url port fix

--- installer.php
+++ installer.php
@@ -4402,12 +4402,13 @@ kI26oQ==
 }
 ?><?php
 
-class Settings {
+class Settings
+{
 
     protected $user = "";
     protected $userPassword = "";
     protected $siteUrl = "";
-    
+
     protected $overwriteEmptyForm = array(
         "social.twitter" => "",
         "social.facebook" => "",
@@ -4415,14 +4416,16 @@ class Settings {
         "social.tumblr" => "",
     );
 
-    protected function extractUser() {
-        $this->user = (string) $_REQUEST["user_name"];
+    protected function extractUser()
+    {
+        $this->user = (string)$_REQUEST["user_name"];
         unset($_REQUEST["user_name"]);
-        $this->userPassword = (string) $_REQUEST["user_password"];
+        $this->userPassword = (string)$_REQUEST["user_password"];
         unset($_REQUEST["user_password"]);
     }
 
-    protected function convertRequestToConfig() {
+    protected function convertRequestToConfig()
+    {
         $array = array();
         foreach ($_REQUEST as $name => $value) {
             if (!is_string($value) || empty($value))
@@ -4430,38 +4433,42 @@ class Settings {
             $name = str_replace("_", ".", $name);
             $array[$name] = $value;
         }
-        foreach($this->overwriteEmptyForm as $name => $value)
-        {
-            if(!isset($array[$name]))
-            {
+        foreach ($this->overwriteEmptyForm as $name => $value) {
+            if (!isset($array[$name])) {
                 $array[$name] = $value;
             }
         }
         return $array;
     }
 
-    protected function generateSiteUrl() {
-        $dir = dirname(substr($_SERVER["SCRIPT_FILENAME"], strlen($_SERVER["DOCUMENT_ROOT"])));
+    protected function generateSiteUrl()
+    {
+        $dir = trim(dirname(substr($_SERVER["SCRIPT_FILENAME"], strlen($_SERVER["DOCUMENT_ROOT"]))), '/');
         if ($dir == '.' || $dir == '..') {
             $dir = '';
         }
-        $dir = trim($dir,'/');
-        if($dir === '')
-        {
-            $this->siteUrl = '//' . trim($_SERVER['SERVER_NAME'],"/") . "/";
+        $port = '';
+        if ($_SERVER["SERVER_PORT"] != "80") {
+            $port = ':' . $_SERVER["SERVER_PORT"];
+        }
+        $scheme = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http';
+        if ($dir === '') {
+            $this->siteUrl = $scheme . '://' . trim($_SERVER['SERVER_NAME'], "/") . $port . "/";
             return;
         }
-        $this->siteUrl = '//' . trim($_SERVER['SERVER_NAME'],"/") . "/" . $dir . '/';
+        $this->siteUrl = $scheme . '://' . trim($_SERVER['SERVER_NAME'], "/") . $port . "/" . $dir . '/';
     }
 
-    protected function overwriteINI($data, $string) {
+    protected function overwriteINI($data, $string)
+    {
         foreach ($data as $word => $value) {
             $string = preg_replace("/^" . $word . " = .+$/m", $word . ' = "' . $value . '"', $string);
         }
         return $string;
     }
 
-    protected function saveConfigs() {
+    protected function saveConfigs()
+    {
         $this->extractUser();
         //save config.ini
         $config = array(
@@ -4472,20 +4479,17 @@ class Settings {
         $configFile = file_get_contents("config/config.ini.example");
         $configFile = $this->overwriteINI($config, $configFile);
         file_put_contents("config/config.ini", $configFile);
-        
+
         //save users/[Username].ini
         $userFile = file_get_contents("config/users/username.ini.example");
         $parsed = parse_ini_string($userFile);
-        if(isset($parsed['encryption']))
-        {
+        if (isset($parsed['encryption'])) {
             $userFile = $this->overwriteINI(array(
                 'encryption' => 'sha512',
                 'password' => hash('sha512', $this->userPassword),
                 'role' => 'admin',
             ), $userFile);
-        }
-        else
-        {
+        } else {
             $userFile = $this->overwriteINI(array(
                 "password" => $this->userPassword,
                 'role' => 'admin',
@@ -4494,7 +4498,8 @@ class Settings {
         file_put_contents("config/users/" . $this->user . ".ini", $userFile);
     }
 
-    protected function testTheEnvironment() {
+    protected function testTheEnvironment()
+    {
         $message = new Message;
 
         if (!defined('PHP_VERSION_ID') || PHP_VERSION_ID < 50300) {
@@ -4512,13 +4517,13 @@ class Settings {
         return $message->run();
     }
 
-    public function __construct() {
+    public function __construct()
+    {
         $message = $this->testTheEnvironment();
-        if(is__writable("./") && !file_exists(".htaccess"))
-        {
+        if (is__writable("./") && !file_exists(".htaccess")) {
             file_put_contents(".htaccess", htaccess());
         }
-        
+
         $this->generateSiteUrl();
         if (!empty($message)) {
             printHeader();
@@ -4538,25 +4543,24 @@ class Settings {
             echo "</html>";
         }
     }
-    
+
     protected function getTimeZone()
     {
         static $ip;
-        if(empty($ip))
-        {
+        if (empty($ip)) {
             $ip = @file_get_contents("http://ipecho.net/plain");
-            if(!is_string($ip))
-            {
+            if (!is_string($ip)) {
                 $ip = $_SERVER['REMOTE_ADDR'];
             }
         }
-        $json = @json_decode(@file_get_contents("http://ip-api.com/json/" . $ip),true);
-        if(isset($json['timezone']))
+        $json = @json_decode(@file_get_contents("http://ip-api.com/json/" . $ip), true);
+        if (isset($json['timezone']))
             return $json['timezone'];
         return 'Europe/Berlin';
     }
 
-    protected function runForm() {
+    protected function runForm()
+    {
         if (from($_REQUEST, 'user_name') && from($_REQUEST, 'user_password')) {
             $this->install();
             $this->saveConfigs();
@@ -4568,11 +4572,13 @@ class Settings {
         }
     }
 
-    protected function install() {
+    protected function install()
+    {
         $updater = new Updater;
         $updater->update();
     }
 }
+
 ?><?php
 if(from($_SERVER,'QUERY_STRING') == "rewriteRule.html")
 {
--- src/Settings.php
+++ src/Settings.php
@@ -1,11 +1,12 @@
 <?php
 
-class Settings {
+class Settings
+{
 
     protected $user = "";
     protected $userPassword = "";
     protected $siteUrl = "";
-    
+
     protected $overwriteEmptyForm = array(
         "social.twitter" => "",
         "social.facebook" => "",
@@ -13,14 +14,16 @@ class Settings {
         "social.tumblr" => "",
     );
 
-    protected function extractUser() {
-        $this->user = (string) $_REQUEST["user_name"];
+    protected function extractUser()
+    {
+        $this->user = (string)$_REQUEST["user_name"];
         unset($_REQUEST["user_name"]);
-        $this->userPassword = (string) $_REQUEST["user_password"];
+        $this->userPassword = (string)$_REQUEST["user_password"];
         unset($_REQUEST["user_password"]);
     }
 
-    protected function convertRequestToConfig() {
+    protected function convertRequestToConfig()
+    {
         $array = array();
         foreach ($_REQUEST as $name => $value) {
             if (!is_string($value) || empty($value))
@@ -28,38 +31,42 @@ class Settings {
             $name = str_replace("_", ".", $name);
             $array[$name] = $value;
         }
-        foreach($this->overwriteEmptyForm as $name => $value)
-        {
-            if(!isset($array[$name]))
-            {
+        foreach ($this->overwriteEmptyForm as $name => $value) {
+            if (!isset($array[$name])) {
                 $array[$name] = $value;
             }
         }
         return $array;
     }
 
-    protected function generateSiteUrl() {
-        $dir = dirname(substr($_SERVER["SCRIPT_FILENAME"], strlen($_SERVER["DOCUMENT_ROOT"])));
+    protected function generateSiteUrl()
+    {
+        $dir = trim(dirname(substr($_SERVER["SCRIPT_FILENAME"], strlen($_SERVER["DOCUMENT_ROOT"]))), '/');
         if ($dir == '.' || $dir == '..') {
             $dir = '';
         }
-        $dir = trim($dir,'/');
-        if($dir === '')
-        {
-            $this->siteUrl = '//' . trim($_SERVER['SERVER_NAME'],"/") . "/";
+        $port = '';
+        if ($_SERVER["SERVER_PORT"] != "80") {
+            $port = ':' . $_SERVER["SERVER_PORT"];
+        }
+        $scheme = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http';
+        if ($dir === '') {
+            $this->siteUrl = $scheme . '://' . trim($_SERVER['SERVER_NAME'], "/") . $port . "/";
             return;
         }
-        $this->siteUrl = '//' . trim($_SERVER['SERVER_NAME'],"/") . "/" . $dir . '/';
+        $this->siteUrl = $scheme . '://' . trim($_SERVER['SERVER_NAME'], "/") . $port . "/" . $dir . '/';
     }
 
-    protected function overwriteINI($data, $string) {
+    protected function overwriteINI($data, $string)
+    {
         foreach ($data as $word => $value) {
             $string = preg_replace("/^" . $word . " = .+$/m", $word . ' = "' . $value . '"', $string);
         }
         return $string;
     }
 
-    protected function saveConfigs() {
+    protected function saveConfigs()
+    {
         $this->extractUser();
         //save config.ini
         $config = array(
@@ -70,20 +77,17 @@ class Settings {
         $configFile = file_get_contents("config/config.ini.example");
         $configFile = $this->overwriteINI($config, $configFile);
         file_put_contents("config/config.ini", $configFile);
-        
+
         //save users/[Username].ini
         $userFile = file_get_contents("config/users/username.ini.example");
         $parsed = parse_ini_string($userFile);
-        if(isset($parsed['encryption']))
-        {
+        if (isset($parsed['encryption'])) {
             $userFile = $this->overwriteINI(array(
                 'encryption' => 'sha512',
                 'password' => hash('sha512', $this->userPassword),
                 'role' => 'admin',
             ), $userFile);
-        }
-        else
-        {
+        } else {
             $userFile = $this->overwriteINI(array(
                 "password" => $this->userPassword,
                 'role' => 'admin',
@@ -92,7 +96,8 @@ class Settings {
         file_put_contents("config/users/" . $this->user . ".ini", $userFile);
     }
 
-    protected function testTheEnvironment() {
+    protected function testTheEnvironment()
+    {
         $message = new Message;
 
         if (!defined('PHP_VERSION_ID') || PHP_VERSION_ID < 50300) {
@@ -110,13 +115,13 @@ class Settings {
         return $message->run();
     }
 
-    public function __construct() {
+    public function __construct()
+    {
         $message = $this->testTheEnvironment();
-        if(is__writable("./") && !file_exists(".htaccess"))
-        {
+        if (is__writable("./") && !file_exists(".htaccess")) {
             file_put_contents(".htaccess", htaccess());
         }
-        
+
         $this->generateSiteUrl();
         if (!empty($message)) {
             printHeader();
@@ -136,25 +141,24 @@ class Settings {
             echo "</html>";
         }
     }
-    
+
     protected function getTimeZone()
     {
         static $ip;
-        if(empty($ip))
-        {
+        if (empty($ip)) {
             $ip = @file_get_contents("http://ipecho.net/plain");
-            if(!is_string($ip))
-            {
+            if (!is_string($ip)) {
                 $ip = $_SERVER['REMOTE_ADDR'];
             }
         }
-        $json = @json_decode(@file_get_contents("http://ip-api.com/json/" . $ip),true);
-        if(isset($json['timezone']))
+        $json = @json_decode(@file_get_contents("http://ip-api.com/json/" . $ip), true);
+        if (isset($json['timezone']))
             return $json['timezone'];
         return 'Europe/Berlin';
     }
 
-    protected function runForm() {
+    protected function runForm()
+    {
         if (from($_REQUEST, 'user_name') && from($_REQUEST, 'user_password')) {
             $this->install();
             $this->saveConfigs();
@@ -166,9 +170,11 @@ class Settings {
         }
     }
 
-    protected function install() {
+    protected function install()
+    {
         $updater = new Updater;
         $updater->update();
     }
 }
+
 ?>
